707cffc91a7137e7c735f4f57a036a9e49849e37
sreepriya1912 <sreepriya.s1912@gmail.com>
2025-07-09 00:38:54 +0530

    Update Order.jsx

--- src/components/sections/Order.jsx
+++ src/components/sections/Order.jsx
@@ -104,17 +104,17 @@ const Order = () => {
             <div className="space-y-4">
               <div className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-amber-600" />
-                <span className="text-gray-700">+91 98765 43210</span>
+                <span className="text-gray-700">+91 94484 87092</span>
               </div>
               
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-amber-600" />
-                <span className="text-gray-700">info@tanjavurart.com</span>
+                <span className="text-gray-700">shreeschoolofarts@gmail.com</span>
               </div>
               
               <div className="flex items-center space-x-3">
                 <MapPin className="w-5 h-5 text-amber-600" />
-                <span className="text-gray-700">Thanjavur, Tamil Nadu, India</span>
+                <span className="text-gray-700">Bengaluru, Karnataka, India</span>
               </div>
             </div>
 
